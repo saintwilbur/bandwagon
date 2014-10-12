f231ad93b04016973e610d7d0fc7dff62917d5c5
kbooras <kbooras@ufl.edu>
2014-10-12 18:51:19 -0400

    Minor text changes, added upcoming events placeholder

--- yolo-octo-bear/app/scripts/controllers/upcomingevents.js
+++ yolo-octo-bear/app/scripts/controllers/upcomingevents.js
@@ -11,7 +11,11 @@ angular.module('yoloOctoBearApp')
   .controller('UpcomingeventsCtrl', function ($scope) {
     $scope.awesomeThings = [
       'Iggy Concert 5/12/14',
-      'Ciara Concert at MSG 6/23/14',
-      'Luke Bryan Signing at Target 7/2/14s'
+      'Ciara at Borders MSG 6/10/14',
+      'Iggy at Starbucks 6/12/14',
+      'Iggy Concert 6/17/14',
+      'Ciara Concert at UF Stadium 6/23/14',
+      'Luke Bryan Signing at Target 7/2/14',
+      'Ciara at Gainesville High School 7/20/14'
     ];
   });
