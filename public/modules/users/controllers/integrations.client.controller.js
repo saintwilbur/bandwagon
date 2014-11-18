6b8e07cf2425c2521148c1804962c386717ff484
kbooras <kbooras@ufl.edu>
2014-11-18 15:27:13 -0500

    made bandwagon.me link to tracked artists page. Changed integrations layout. Began creating integrations controller

--- public/modules/users/controllers/integrations.client.controller.js
+++ public/modules/users/controllers/integrations.client.controller.js
@@ -1,8 +1,27 @@
 'use strict';
 
-angular.module('users').controller('IntegrationsController', ['$scope',
-	function($scope) {
+angular.module('users').controller('IntegrationsController', ['$scope', '$http', '$location', 'Users', 'Authentication',
+	function($scope, $http, $location, Users, Authentication) {
 		// Integrations controller logic
 		// ...
+
+		$scope.user = Authentication.user;
+
+		// If user is not signed in then redirect back home
+		if (!$scope.user) $location.path('/');
+
+		// Check if there are linked music accounts 
+		$scope.hasConnectedMusicAccounts = function(provider) {
+			for (var i in $scope.user.additionalProvidersData) {
+				return true;
+			}
+
+			return false;
+        };
+
+        // Check if provider is already in use with current user
+		$scope.isConnectedMusicAccount = function(provider) {
+			return $scope.user.provider === provider || ($scope.user.additionalProvidersData && $scope.user.additionalProvidersData[provider]);
+		};
 	}
 ]);
